b8ffe365fd069aadff6b76d6ae62b075d90647b3
Luis Felipe Velasco Lopez <luis.velasco@ucb.edu.bo>
2025-02-27 10:02:57 -0400

    interface terminada empezamos funcionalidades

---
+++ src/calculador.test.js
@@ -0,0 +1,7 @@
+//import sumar from "./sumador.js";
+
+// describe("totalizar", () => {
+//   it("deberia multiplicar dos numeros", () => {
+//     expect(totalizar(20, 3)).toEqual(60);
+//   });
+// });
