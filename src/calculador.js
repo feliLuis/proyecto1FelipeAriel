31776a2d2f12f66282bf1836caf976488844d6cc
Luis Felipe Velasco Lopez <luis.velasco@ucb.edu.bo>
2025-02-27 10:10:18 -0400

    primera funcionalidad lista totalizar

---
+++ src/calculador.js
@@ -0,0 +1,5 @@
+function totalizar(a, b) {
+    return a * b;
+  }
+  
+  export default totalizar;
--- src/calculador.test.js
+++ src/calculador.test.js
@@ -1,7 +1,7 @@
-//import sumar from "./sumador.js";
+import totalizar from "./calculador.js";
 
-// describe("totalizar", () => {
-//   it("deberia multiplicar dos numeros", () => {
-//     expect(totalizar(20, 3)).toEqual(60);
-//   });
-// });
+describe("totalizar", () => {
+  it("deberia multiplicar dos numeros", () => {
+    expect(totalizar(20, 3)).toEqual(60);
+  });
+});
